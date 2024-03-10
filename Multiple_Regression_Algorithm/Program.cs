c6fcc3ae9a2eefa9da4f40cc3e89fa9d9089aa8d
HHristova <hristiana_h@yahoo.com>
2024-03-10 17:40:21 +0200

    Refactoring, unit tests for the methods, more test data

--- Multiple_Regression_Algorithm/Program.cs
+++ Multiple_Regression_Algorithm/Program.cs
@@ -1,182 +1,21 @@
-﻿using System.Reflection.Metadata.Ecma335;
-
+﻿
 namespace Multiple_Regression_Algorithm
 {
     public static class Gauss
     {
         static void Main(string[] args)
         {
-            // CHECK HOW MANY INDEPENDENT VARIABLES ARE INPUTED... MUST BE MORE THAN 2
-            // CHECK THE FILE HANDLING
-
-            Console.WriteLine("Enter the number of independent variables: ");
-           // int numIndependentVars = int.Parse(Console.ReadLine());
-            // HARDCODED -------->>>>>>>>>>>>>>>> REMOVE LATER
-            int numIndependentVars = 6;
-
             Console.WriteLine("Enter the path to the data file: ");
             //string filePath = Console.ReadLine();
             // HARDCODED -------->>>>>>>>>>>>>>>> REMOVE LATER
-            string filePath = "Test_Data_1.txt";
+            string filePath = "Test_Data_3.txt";
 
             // Read data from the file
-            double[] result = RegressionAlgorithmFromFile(filePath);
+            double[] result = MultipleRegression.RegressionAlgorithmFromFile(filePath);
             for (int i = 0; i < result.Length; i++)
             {
                 Console.WriteLine("B" + i + ": " + result[i]);
             }
-        }
-
-        public static double[] RegressionAlgorithmFromFile(string filePath)
-        {
-            double[][] inputData = ReadDataFromFile(filePath);
-            return RegressionAlgorithm(inputData, inputData[0].Length, inputData.Length);
-        }
-
-        // Function to perform Gaussian elimination and back substitution
-        public static double[] RegressionAlgorithm(double[][] data, int numberOfVariables, int numberOfSamples)
-        {
-            // Solve the linear equations using the sums
-            double[][] matrix = ConstructMatrix(data, numberOfVariables, numberOfSamples);
-
-            // Gaussian elimination for reducing a matrix to row-echelon form.
-            ReduceMatrixToRowForm(matrix, numberOfVariables);
-
-            // Solving the system of linear equations represented by the row-echelon matrix.
-            double[] resultCoeff = new double[numberOfVariables];
-            for (int i = 0; i < resultCoeff.Length; i++)
-            {
-                resultCoeff[i] = 1f;
-            }
-            resultCoeff = BackSubstitution(matrix, resultCoeff, numberOfVariables);
-
-            // Round
-            for (int i = 0; i < resultCoeff.Length; i++)
-            {
-                resultCoeff[i] = Math.Round(resultCoeff[i], 4);
-            }
-
-            return resultCoeff;
-        }
-
-        public static double[][] ReadDataFromFile(string filePath)
-        {
-            string[] lines = File.ReadAllLines(filePath);
-
-            int numEntries = lines[0].Split(',').Length;
-            double[][] fileData = new double[numEntries][];
-            for (int i = 0; i < numEntries; i++)
-            {
-                fileData[i] = new double[lines.Length];
-            }
-
-            for (int i = 0; i < lines.Length; i++)
-            {
-                string[] values = lines[i].Split(',');
-                int lineEntries = values.Length;
-                for (int v = 0; v < lineEntries; v++)
-                {
-                    fileData[v][i] = double.Parse(values[v]);
-                }
-            }
-
-            return fileData;
-        }
-
-        public static double[][] ConstructMatrix(double[][] data, int numberOfVariables, int numberOfSamples)
-        {
-            double[][] matrix = new double[numberOfVariables][];
-            for (int i = 0; i < numberOfVariables; i++)
-            {
-                int currentVariable = i - 1;
-                double[] row = new double[numberOfVariables + 1];
-                if (i == 0)
-                {
-                    row[0] = numberOfSamples;
-                }
-                else
-                {
-                    double sum = 0;
-                    for (int s = 0; s < numberOfSamples; s++)
-                    {
-                        sum += data[s][currentVariable];
-                    }
-                    row[0] = sum;
-                }
-
-                for (int c = 0; c < numberOfVariables; c++)
-                {
-                    double sum = 0;
-                    for (int s = 0; s < numberOfSamples; s++)
-                    {
-                        double currentVarSample = i == 0 ? 1 : data[s][currentVariable];
-                        sum += data[s][c] * currentVarSample;
-                    }
-                    row[c + 1] = sum;
-                }
-
-                matrix[i] = row;
-            }
-
-            return matrix;
-        }
-
-        // Eliminating variables below the main diagonal to transform the matrix into an upper triangular form
-        public static void ReduceMatrixToRowForm(double[][] matrix, int numberOfVariables)
-        {
-            for (int v = 0; v < numberOfVariables - 1; v++)
-            {
-                var varSimplifying = v;
-                var firstRow = matrix[v];
-                var firstRowValue = firstRow[varSimplifying];
-
-                for (int r = v + 1; r < matrix.Length; r++)
-                {
-                    var myRow = matrix[r];
-                    var targetValue = myRow[varSimplifying];
-                    double coeff = targetValue / firstRowValue;
-
-                    for (int c = 0; c < firstRow.Length; c++)
-                    {
-                        myRow[c] = myRow[c] - (firstRow[c] * coeff);
-                    }
-                }
-            }
-        }
-
-        // Calculating the coefficients of the regression model using back substitution
-        public static double[] BackSubstitution(double[][] matrix, double[] resultCoeff, int numberOfVariables)
-        {
-            for (int i = numberOfVariables - 1; i >= 0; i--)
-            {
-                var row = matrix[i];
-
-                for (int c = 0; c < resultCoeff.Length; c++)
-                {
-                    row[c] = row[c] * resultCoeff[c];
-                }
-
-                double firstNonZero = 0;
-                int firstNonZeroIndex = 0;
-                for (int c = 0; c < row.Length; c++)
-                {
-                    if (row[c] != 0)
-                    {
-                        firstNonZero = row[c];
-                        firstNonZeroIndex = c;
-                        break;
-                    }
-                }
-
-                double target = row[numberOfVariables];
-                for (int c = firstNonZeroIndex + 1; c < row.Length - 1; c++)
-                {
-                    target -= row[c];
-                }
-
-                resultCoeff[i] = target / firstNonZero;
-            }
-            return resultCoeff;
         }
     }
 }
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,43 +1,374 @@
 using Multiple_Regression_Algorithm;
-using static Multiple_Regression_Algorithm.Gauss;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        #region Functioal Tests
+        // TESTS ENTIRE FUNCTIONALITY
+        // FUNCTIONAL TESTS: Test 1,Test 2, Test 3
 
         // Data from Table 1 as Input
         // Data from Table 2 as Output
         [Test]
-        public void Test1()
+        public void RegressionAlgorithmFromFileTestfile1()
         {
-            double[] result = RegressionAlgorithmFromFile("Test_Data_1.txt");
+            double[] result = MultipleRegression.RegressionAlgorithmFromFile("Test_Data_1.txt");
 
             Assert.That(result[0], Is.EqualTo(0.5665));
             Assert.That(result[1], Is.EqualTo(0.0653));
             Assert.That(result[2], Is.EqualTo(0.0087));
             Assert.That(result[3], Is.EqualTo(0.1510));
-
-            //Assert.Pass();
         }
 
         // Data from Table 3 as Input
         // Data from Table 4 as Output
         [Test]
-        public void Test2()
+        public void RegressionAlgorithmFromFileTestfile2()
         {
-            double[] result = RegressionAlgorithmFromFile("Test_Data_2.txt");
+            double[] result = MultipleRegression.RegressionAlgorithmFromFile("Test_Data_2.txt");
 
             Assert.That(result[0], Is.EqualTo(6.7013));
             Assert.That(result[1], Is.EqualTo(0.0784));
             Assert.That(result[2], Is.EqualTo(0.015));
             Assert.That(result[3], Is.EqualTo(0.2461));
-
-            //Assert.Pass();
         }
+
+        // Data from file 3
+        [Test]
+        public void RegressionAlgorithmFromFileTestfile3()
+        {
+            double[] result = MultipleRegression.RegressionAlgorithmFromFile("Test_Data_3.txt");
+
+            Assert.That(result[0], Is.EqualTo(-0.8));
+            Assert.That(result[1], Is.EqualTo(2.1333));
+            Assert.That(result[2], Is.EqualTo(-1.1667));
+            Assert.That(result[3], Is.EqualTo(0.3667));
+        }
+
+        #endregion
+
+        #region Unit Tests
+        // TESTS INDIVIDUAL METHODS:
+        // TESTING ReadDataFromFile
+        [Test]
+        public void ReadDataFromFileTestfile1()
+        {
+            double[][] expectedData =
+            {
+                new double[] { 345, 65, 23, 31.4 },
+                new double[] { 168, 18, 18, 14.6 },
+                new double[] { 94, 0, 0, 6.4 },
+                new double[] { 187, 185, 98, 28.3 },
+                new double[] { 621, 87, 10, 42.1 },
+                new double[] { 255, 0, 0, 15.3 }
+            };
+
+            double[][] actualData = MultipleRegression.ReadDataFromFile("Test_Data_1.txt");
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+            Assert.That(actualData[4], Is.EqualTo(expectedData[4]));
+            Assert.That(actualData[5], Is.EqualTo(expectedData[5]));
+        }
+
+        [Test]
+        public void ReadDataFromFileTestfile2()
+        {
+            double[][] expectedData =
+            {
+                new double[] { 1142, 1060, 325, 201 },
+                new double[] { 863, 995, 98, 98 },
+                new double[] { 1065, 3205, 23, 162 },
+                new double[] { 554, 120, 0, 54 },
+                new double[] { 983, 2896, 120, 138 },
+                new double[] { 256, 485, 88, 61 }
+            };
+            
+            double[][] actualData = MultipleRegression.ReadDataFromFile("Test_Data_2.txt");
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+            Assert.That(actualData[4], Is.EqualTo(expectedData[4]));
+            Assert.That(actualData[5], Is.EqualTo(expectedData[5]));
+        }
+
+        [Test]
+        public void ReadDataFromFileTestfile3()
+        {
+            double[][] expectedData =
+            {
+                new double[] { 1, 0, 4, 2 },
+                new double[] { 1, 2, 6, 2 },
+                new double[] { 2, 0, 8, 8 },
+                new double[] { 2, 1, 9, 4 },
+                new double[] { 1, 0, 10, 5 },
+                new double[] { 1, 0, 10, 5 }
+            };
+
+            double[][] actualData = MultipleRegression.ReadDataFromFile("Test_Data_3.txt");
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+            Assert.That(actualData[4], Is.EqualTo(expectedData[4]));
+            Assert.That(actualData[5], Is.EqualTo(expectedData[5]));
+        }
+
+
+        // TESTING ConstructMatrix
+        [Test]
+        public void ConstructMatrixTestfile1()
+        {
+            double[][] expectedData =
+            {
+                new double[] { 6, 1670, 355, 149, 138.10000000000002 },
+                new double[] { 1670, 641720, 114071, 35495, 49225.100000000006 },
+                new double[] { 355, 114071, 46343, 20819, 11202 },
+                new double[] { 149, 35495, 20819, 10557, 4179.4 }
+            };
+
+            double[][] inputData =
+            {
+                new double[] { 345, 65, 23, 31.4 },
+                new double[] { 168, 18, 18, 14.6 },
+                new double[] { 94, 0, 0, 6.4 },
+                new double[] { 187, 185, 98, 28.3 },
+                new double[] { 621, 87, 10, 42.1 },
+                new double[] { 255, 0, 0, 15.3 }
+            };
+
+            double[][] actualData = MultipleRegression.ConstructMatrix(inputData, 4, 6);
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void ConstructMatrixTestfile2()
+        {
+            double[][] expectedData =
+            {
+                new double[] { 6, 4863, 8761, 654, 714 },
+                new double[] { 4863, 4521899, 8519938, 620707, 667832},
+                new double[] { 8761, 8519938, 21022091, 905925, 1265493 },
+                new double[] { 654, 620707, 905925, 137902, 100583 }
+            };
+
+            double[][] inputData =
+            {
+                new double[] { 1142, 1060, 325, 201 },
+                new double[] { 863, 995, 98, 98 },
+                new double[] { 1065, 3205, 23, 162 },
+                new double[] { 554, 120, 0, 54 },
+                new double[] { 983, 2896, 120, 138 },
+                new double[] { 256, 485, 88, 61 }
+            };
+
+            double[][] actualData = MultipleRegression.ConstructMatrix(inputData, 4, 6);
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void ConstructMatrixTestfile3()
+        {
+            double[][] expectedData =
+           {
+                new double[] { 6, 8, 3, 47, 26 },
+                new double[] { 8, 12, 4, 64, 38 },
+                new double[] { 3, 4, 5, 21, 8 },
+                new double[] { 47, 64, 21, 397, 220 }
+            };
+            
+            double[][] inputData =
+            {
+                new double[] { 1, 0, 4, 2 },
+                new double[] { 1, 2, 6, 2 },
+                new double[] { 2, 0, 8, 8 },
+                new double[] { 2, 1, 9, 4 },
+                new double[] { 1, 0, 10, 5 },
+                new double[] { 1, 0, 10, 5 }
+            };
+
+            double[][] actualData = MultipleRegression.ConstructMatrix(inputData, 4, 6);
+
+            Assert.AreEqual(expectedData.Length, actualData.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+
+        // TESTING ReduceMatrixToRowForm:
+        [Test]        
+        public void ReduceMatrixToRowFormTestfile1()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 1670, 355, 149, 138.10000000000002 },
+                new double[] { 1670, 641720, 114071, 35495, 49225.100000000006 },
+                new double[] { 355, 114071, 46343, 20819, 11202 },
+                new double[] { 149, 35495, 20819, 10557, 4179.4 }
+            };
+
+            double[][] expectedData =
+            {
+                new double[] { 6, 1670, 355, 149, 138.10000000000002 },
+                new double[] { 0, 176903.33333333337, 15262.666666666672, - 5976.666666666664, 10787.26666666667 },
+                new double[] { 0, 0, 24022.018158693074, 12518.814785852916, 2100.391682274687 },
+                new double[] { 0, 0, 0, 130.86722946170175, 19.76731802670497 },
+            };
+
+            MultipleRegression.ReduceMatrixToRowForm(matrix, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(matrix[0], Is.EqualTo(expectedData[0]));
+            Assert.That(matrix[1], Is.EqualTo(expectedData[1]));
+            Assert.That(matrix[2], Is.EqualTo(expectedData[2]));
+            Assert.That(matrix[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void ReduceMatrixToRowFormTestfile2()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 4863, 8761, 654, 714 },
+                new double[] { 4863, 4521899, 8519938, 620707, 667832 },
+                new double[] { 8761, 8519938, 21022091, 905925, 1265493 },
+                new double[] { 654, 620707, 905925, 137902, 100583 }
+            };
+            
+            double[][] expectedData =
+            {
+                new double[] { 6, 4863, 8761, 654, 714 },
+                new double[] { 0, 580437.5, 1419147.5, 90640, 89135 },
+                new double[] { 0, 0, 4759809.443422704, - 270635.3352428125, 5002.332227845356 },
+                new double[] { 0, 0, 0, 37073.92977685743, 9122.275195227476 },
+            };
+
+            MultipleRegression.ReduceMatrixToRowForm(matrix, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(matrix[0], Is.EqualTo(expectedData[0]));
+            Assert.That(matrix[1], Is.EqualTo(expectedData[1]));
+            Assert.That(matrix[2], Is.EqualTo(expectedData[2]));
+            Assert.That(matrix[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void ReduceMatrixToRowFormTestfile3()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 8, 3, 47, 26 },
+                new double[] { 8, 12, 4, 64, 38 },
+                new double[] { 3, 4, 5, 21, 8 },
+                new double[] { 47, 64, 21, 397, 220 }
+            };
+
+            double[][] expectedData =
+            {
+                new double[] { 6, 8, 3, 47, 26 },
+                new double[] { 0, 1.333333333333334, 0, 1.3333333333333357, 3.3333333333333357 },
+                new double[] { 0, 0, 3.5, - 2.5, - 5 },
+                new double[] { 0, 0, 0, 25.71428571428575, 9.428571428571432 },
+            };
+
+            MultipleRegression.ReduceMatrixToRowForm(matrix, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(matrix[0], Is.EqualTo(expectedData[0]));
+            Assert.That(matrix[1], Is.EqualTo(expectedData[1]));
+            Assert.That(matrix[2], Is.EqualTo(expectedData[2]));
+            Assert.That(matrix[3], Is.EqualTo(expectedData[3]));
+        }
+
+
+        // TESTING BackSubstitution:
+        [Test]
+        public void BackSubstitutionTsetfile1()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 1670, 355, 149, 138.10000000000002 },
+                new double[] { 0, 176903.33333333337, 15262.666666666672, - 5976.666666666664, 10787.26666666667 },
+                new double[] { 0, 0, 24022.018158693074, 12518.814785852916, 2100.391682274687 },
+                new double[] { 0, 0, 0, 130.86722946170175, 19.76731802670497 }
+            };
+
+            double[] expectedData = { 0.5664574696019447, 0.06532925469423313, 0.008718736194577725, 0.151048647610362 };
+
+            double[] actualData = MultipleRegression.BackSubstitution(matrix, new double[] {1, 1, 1, 1}, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void BackSubstitutionTsetfile2()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 4863, 8761, 654, 714 },
+                new double[] { 0, 580437.5, 1419147.5, 90640, 89135 },
+                new double[] { 0, 0, 4759809.443422704, - 270635.3352428125, 5002.332227845356 },
+                new double[] { 0, 0, 0, 37073.92977685743, 9122.275195227476 }
+            };
+            
+            double[] expectedData = { 6.701336536389111, 0.07836603673386544, 0.015041331199344947, 0.24605633258014775 };
+
+            double[] actualData = MultipleRegression.BackSubstitution(matrix, new double[] { 1, 1, 1, 1 }, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+        [Test]
+        public void BackSubstitutionTsetfile3()
+        {
+            double[][] matrix =
+            {
+                new double[] { 6, 8, 3, 47, 26 },
+                new double[] { 0, 1.333333333333334, 0, 1.3333333333333357, 3.3333333333333357 },
+                new double[] { 0, 0, 3.5, - 2.5, - 5 },
+                new double[] { 0, 0, 0, 25.71428571428575, 9.428571428571432 }
+            };
+
+            double[] expectedData = { -0.7999999999999977, 2.1333333333333337, - 1.1666666666666667, 0.3666666666666663 };
+
+            double[] actualData = MultipleRegression.BackSubstitution(matrix, new double[] { 1, 1, 1, 1 }, 4);
+
+            Assert.AreEqual(expectedData.Length, matrix.Length);
+            Assert.That(actualData[0], Is.EqualTo(expectedData[0]));
+            Assert.That(actualData[1], Is.EqualTo(expectedData[1]));
+            Assert.That(actualData[2], Is.EqualTo(expectedData[2]));
+            Assert.That(actualData[3], Is.EqualTo(expectedData[3]));
+        }
+
+        #endregion
     }
 }
